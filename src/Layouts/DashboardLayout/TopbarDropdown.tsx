0120c143b611f58e828d8777a806f0084568a5d8
Geriano <gerznewbie@gmail.com>
2023-02-25 22:23:15 +0700

    fix bug topbar dropdown not closed

--- src/Layouts/DashboardLayout/TopbarDropdown.tsx
+++ src/Layouts/DashboardLayout/TopbarDropdown.tsx
@@ -11,6 +11,8 @@ export default function TopbarDropdown() {
   const active = useAppSelector(state => state.layout.theme)
   const dispatch = useAppDispatch()
 
+  const close = () => setTimeout(() => dispatch(toggle('dropdown')), 150)
+
   return (
     <div className="relative flex-none w-14 h-14 flex items-center justify-center">
       <button onClick={() => dispatch(toggle('dropdown'))} type="button" title="Toggle Dropdown" className={classNames("transition-all duration-300", {
@@ -26,19 +28,19 @@ export default function TopbarDropdown() {
         'bg-white text-gray-700': active !== 'dark',
         'bg-gray-800 text-gray-100 border-gray-900': active == 'dark',
       })}>
-        <div className={classNames("px-2 transition-all duration-300 hover:bg-gray-50", { 'hover:bg-gray-900': active === 'dark' })}>
+        <div onClick={close} className={classNames("px-2 transition-all duration-300 hover:bg-gray-50", { 'hover:bg-gray-900': active === 'dark' })}>
           <Link to="/profile" className={classNames("flex items-center space-x-2 border-b px-4 py-2", { 'border-gray-900': active === 'dark' })} title="Profile">
             <Icon path={mdiAccount} size={.75} />
             <p className="capitalize font-medium">profile</p>
           </Link>
         </div>
-        <div className={classNames("px-2 transition-all duration-300 hover:bg-gray-50", { 'hover:bg-gray-900': active === 'dark' })}>
+        <div onClick={close} className={classNames("px-2 transition-all duration-300 hover:bg-gray-50", { 'hover:bg-gray-900': active === 'dark' })}>
           <Link to="/setting" className={classNames("flex items-center space-x-2 border-b px-4 py-2", { 'border-gray-900': active === 'dark' })} title="Setting">
             <Icon path={mdiCog} size={.75} />
             <p className="capitalize font-medium">setting</p>
           </Link>
         </div>
-        <div className={classNames("px-2 transition-all duration-300 hover:bg-gray-50 border-b", { 'border-gray-900 hover:bg-gray-900': active === 'dark' })}>
+        <div onClick={close} className={classNames("px-2 transition-all duration-300 hover:bg-gray-50 border-b", { 'border-gray-900 hover:bg-gray-900': active === 'dark' })}>
           <button onClick={e => dispatch(logout())} type="button" className="flex items-center space-x-2 px-4 py-2" title="Logout">
             <Icon path={mdiLogout} size={.75} />
             <p className="capitalize font-medium">logout</p>
